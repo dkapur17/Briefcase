6204d2076cb533be82249e9739b85e05cefbce7c
Dhruv Kapur <dhruvkapur4@gmail.com>
2021-01-15 20:34:10 +0530

    Added search, sort and filter to job dashboard

--- frontend/src/components/Applicants/JobDasboard.js
+++ frontend/src/components/Applicants/JobDasboard.js
@@ -10,7 +10,13 @@ import JobCard from './subcomponents/JobCard';
 const JobDashboard = () => {
     const { userData } = useContext(UserContext);
     const [allActiveJobs, setAllActiveJobs] = useState([]);
+    const [visibleJobs, setVisibleJobs] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [searchString, setSearchString] = useState("");
+    const [sortStyle, setSortStyle] = useState({ attr: "none", order: "asc" });
+    const [jobType, setJobType] = useState("any");
+    const [salaryRange, setSalaryRange] = useState([0, Infinity]);
+    const [durationUpperLimit, setDurationUpperLimit] = useState(7);
     const history = useHistory();
 
     useEffect(() => {
@@ -20,6 +26,7 @@ const JobDashboard = () => {
             try {
                 const response = await axios.get('/api/applicant/getActiveJobs', { headers: { 'auth-token': userData.token } });
                 setAllActiveJobs(response.data.jobs);
+                setVisibleJobs(response.data.jobs);
                 setLoading(false);
             }
             catch (err) {
@@ -29,13 +36,99 @@ const JobDashboard = () => {
         };
         getActiveJobs();
     }, [userData.token, history, userData.user.type]);
+
+    useEffect(() => {
+        // Search
+        const searchRE = new RegExp(searchString ? searchString.toLowerCase() : ".*");
+        let filteredList = allActiveJobs.filter(job => searchRE.test(job.title.toLowerCase()));
+        // JobType
+        filteredList = filteredList.filter(job => jobType === "any" ? true : job.jobType === jobType);
+
+        // Salary
+        filteredList = filteredList.filter(job => job.salary >= salaryRange[0] && job.salary <= salaryRange[1]);
+
+        // Duration
+        filteredList = filteredList.filter(job => job.duration < durationUpperLimit);
+
+        // Sort
+        filteredList.sort((a, b) => {
+            const coeff = sortStyle.order === "asc" ? 1 : -1;
+            if (sortStyle.attr === "salary")
+                return coeff * (a.salary - b.salary);
+            else if (sortStyle.attr === "duration")
+                return coeff * (a.duration - b.duration);
+        });
+
+        setVisibleJobs(filteredList);
+    }, [searchString, jobType, salaryRange, durationUpperLimit, sortStyle]);
+
     return (
         loading ? <FullPageSpinner />
             :
             <div className="container mt-3 mb-5">
                 <h1 className='text-center mb-5'>New Job Listings</h1>
+                <div className="row mb-4">
+                    <div className="card col-12">
+                        <div className="card-body">
+                            <p className="lead text-center">Search, Sort and Filter</p>
+                            <div className="form-group">
+                                <label htmlFor="titleSeacrch"><strong>Search for a job title: </strong></label>
+                                <input type="text" id="titleSearch" className='form-control' value={searchString} onChange={({ target }) => setSearchString(target.value)} />
+                            </div>
+                            <div className="row">
+                                <div className="form-group col-3">
+                                    <label htmlFor="jobType">Job Type</label>
+                                    <select value={jobType} onChange={({ target }) => setJobType(target.value)} className="custom-select">
+                                        <option value="any">Any</option>
+                                        <option value="fullTime">Full Time</option>
+                                        <option value="partTime">Part Time</option>
+                                        <option value="workFromHome">Work From Home</option>
+                                    </select>
+                                </div>
+                                <div className="form-group col-3">
+                                    <label htmlFor="salaryRange">Salary</label>
+                                    <div className="row justify-content-around">
+                                        <input type="number" placeholder='Min' className='form-control col-5' value={salaryRange[0] === 0 ? "" : salaryRange[0]} onChange={({ target }) => setSalaryRange([target.value === "" ? 0 : Number(target.value), salaryRange[1]])} />
+                                        <input type="number" placeholder='Max' className='form-control col-5' value={salaryRange[1] === Infinity ? "" : salaryRange[1]} onChange={({ target }) => setSalaryRange([salaryRange[0], target.value === "" ? Infinity : Number(target.value)])} />
+                                    </div>
+                                </div>
+                                <div className="form-group col-2">
+                                    <label htmlFor="duartion">Duration Upper Limit</label>
+                                    <select value={durationUpperLimit} onChange={({ target }) => setDurationUpperLimit(Number(target.value))} className="custom-select">
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                    </select>
+                                </div>
+                                <div className="form-group col-4">
+                                    <label htmlFor="sort">Sorting Options</label>
+                                    <div className="row justify-content-around">
+                                        <select value={sortStyle.attr} onChange={({ target }) => setSortStyle({ ...sortStyle, attr: target.value })} className="custom-select col-5">
+                                            <option value="none">None</option>
+                                            <option value="salary">Salary</option>
+                                            <option value="duration">Duration</option>
+                                            <option value="rating">Rating</option>
+                                        </select>
+                                        <select value={sortStyle.order} onChange={({ target }) => setSortStyle({ ...sortStyle, order: target.value })} className="custom-select col-5">
+                                            <option value="asc">Ascending</option>
+                                            <option value="dsc">Descending</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="row justify-content-center">
-                    {allActiveJobs.map(job => <JobCard job={job} key={job._id} />)}
+                    {
+                        visibleJobs.length ?
+                            visibleJobs.map(job => <JobCard job={job} key={job._id} />) :
+                            <h1 className='display-3 text-center'>Looks like there aren't any jobs matching the search criteria 😢</h1>
+                    }
                 </div>
             </div>
     )
